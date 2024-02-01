baa9ffb3e2a338e21f867b3b85138859678d933d
Ramyakrishnapitaka <154577647+Ramyakrishnapitaka@users.noreply.github.com>
2024-02-01 16:52:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var score=0;
+function decrease(){
+    score=score-1;
+    document.getElementById("box").textContent=score;
+}
+function increase(){
+    score=score+1;
+    document.getElementById("box").textContent=score;
+}
+function Show(){
+    document.getElementById("hide").style.display="block";
+     
+    
+}
+function Second(){
+    document.getElementById("Hide1").style.display="block";
+     
+    
+}
+function Third(){
+    document.getElementById("Hide2").style.display="block";
+     
+    
+}
+function Forth(){
+    document.getElementById("Hide3").style.display="block";
+     
+    
+}
+
+
